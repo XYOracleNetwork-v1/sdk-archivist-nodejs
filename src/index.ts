3b4f7488e3b24c691e45b36ea986cf7e69ea3ebe
Arie Trouw <arie.trouw@xyfindables.com>
2019-04-17 15:02:29 -0700

    Build Fixed

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+/*
+ * File: index.ts
+ * Project: @xyo-network/sdk-archivist-nodejs
+ * File Created: Wednesday, 17th April 2019 2:51:11 pm
+ * Author: XYO Development Team (support@xyo.network)
+ * -----
+ * Last Modified: Wednesday, 17th April 2019 2:57:14 pm
+ * Modified By: XYO Development Team (support@xyo.network>)
+ * -----
+ * Copyright 2017 - 2019 XY - The Persistent Company
+ */
+
+export { IXyoArchivistNetwork, XyoArchivistNetwork } from './network/'
+export { createArchivistDynamoRepository, IDynamoArchivistRepositoryConfig } from './repository/dynamo'
+export { createArchivistLevelRepository, ILevelArchivistRepositoryConfig } from './repository/level'
+export { createArchivistNeo4jRepository, INeo4jArchivistRepositoryConfig } from './repository/neo4j'
+export { createArchivistSqlRepository, ISqlArchivistRepositoryConfig } from './repository/sql'
+export {
+  IXyoArchivistRepository,
+  IXyoEntitiesList,
+  IXyoEntity,
+  IXyoEntityType,
+  IXyoOriginBlockResult,
+  IXyoOriginBlocksByPublicKeyResult,
+  IXyoIntersectionsList,
+  IArchivistRepositoryConfig
+} from './repository'
