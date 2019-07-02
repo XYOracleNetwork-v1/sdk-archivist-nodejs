25b7e6381d3abd238b43fdc661345467796636c3
Carter Harrison <carterjharrison@gmail.com>
2019-07-01 19:32:35 -0700

    use wss not ws

---
+++ src/notify/xyo-websocket-notify.ts
@@ -0,0 +1,134 @@
+import { IXyoPlugin, IXyoGraphQlDelegate, IXyoPluginDelegate, XyoPluginProviders, XyoBase } from '@xyo-network/sdk-base-nodejs'
+import { XyoBoundWitnessInserter, XyoObjectSchema, XyoBoundWitness, XyoIterableStructure, XyoStructure, XyoSchema, gpsResolver, XyoSha256 } from '@xyo-network/sdk-core-nodejs'
+import ngeohash from 'ngeohash'
+import bs58 from 'bs58'
+import ws from 'ws'
+import https from 'https'
+import fs from 'fs'
+
+class XyoWebsocketNotify extends XyoBase implements IXyoPlugin {
+  private clients : {[key: string]: ws} = {}
+  private client: ws.Server | undefined
+
+  public getName(): string {
+    return 'notify-on-location'
+  }
+
+  public getProvides(): string[] {
+    return []
+  }
+  public getPluginDependencies(): string[] {
+    return [
+      XyoPluginProviders.BOUND_WITNESS_INSERTER
+    ]
+  }
+
+  private onConnection = (connection : ws) => {
+    this.clients[connection.url] = connection
+
+    const key = Math.random().toString()
+
+    connection.on('close', () => {
+      delete this.clients[key]
+      connection.close()
+    })
+
+    connection.on('error', () => {
+      delete this.clients[key]
+      connection.close()
+    })
+
+    connection.on('unexpected-response', () => {
+      delete this.clients[key]
+      connection.close()
+    })
+  }
+
+  public async initialize(delegate: IXyoPluginDelegate): Promise<boolean> {
+    const inserter = delegate.deps.BOUND_WITNESS_INSERTER as XyoBoundWitnessInserter
+
+    const server = https.createServer({
+      cert: fs.readFileSync(delegate.config.cert),
+      key: fs.readFileSync(delegate.config.key)
+    })
+
+    this.client = new ws.Server({ server })
+    this.client.on('connection', this.onConnection)
+
+    server.listen(11002)
+
+    inserter.addBlockListener('notify-on-location', async(boundWitness) => {
+      const bridgeBlocks = this.getNestedObjectType(new XyoBoundWitness(boundWitness), XyoObjectSchema.WITNESS, XyoObjectSchema.BRIDGE_BLOCK_SET)
+
+      await this.sendBlock(new XyoBoundWitness(boundWitness), false)
+
+      if (bridgeBlocks) {
+        const it = (bridgeBlocks as XyoIterableStructure).newIterator()
+
+        while (it.hasNext()) {
+          await this.sendBlock(new XyoBoundWitness(it.next().value.getAll()), true)
+        }
+      }
+
+    })
+
+    return true
+  }
+
+  private async sendBlock(block: XyoBoundWitness, isBridge: boolean) {
+    const geohash = this.getGeohash(block)
+
+    if (geohash) {
+      const hash = bs58.encode(block.getHash(new XyoSha256()).getAll().getContentsCopy())
+      console.log(hash)
+
+      for (const url of Object.keys(this.clients)) {
+        const socket = this.clients[url]
+
+        socket.send(JSON.stringify({
+          hash,
+          geohash,
+          isBridge
+        }))
+      }
+    }
+  }
+
+  private getNestedObjectType(boundWitness: XyoBoundWitness, rootSchema: XyoSchema, subSchema: XyoSchema): XyoStructure | undefined {
+    const it = boundWitness.newIterator()
+
+    while (it.hasNext()) {
+      const bwItem = it.next().value
+
+      if (bwItem.getSchema().id === rootSchema.id && bwItem instanceof XyoIterableStructure) {
+        const fetterIt = bwItem.newIterator()
+
+        while (fetterIt.hasNext()) {
+          const fetterItem = fetterIt.next().value
+
+          if (fetterItem.getSchema().id === subSchema.id) {
+            return fetterItem
+          }
+        }
+      }
+    }
+
+    return
+  }
+
+  private getGeohash(boundWitness: XyoBoundWitness): string | undefined {
+    for (const party of boundWitness.getHeuristics()) {
+      for (const huerestic of party) {
+
+        if (huerestic.getSchema().id === XyoObjectSchema.GPS.id) {
+          const point = gpsResolver.resolve(huerestic.getAll().getContentsCopy()).value
+          const geohash = ngeohash.encode(point.lat, point.lng)
+        // this.logInfo(`Adding geohash: ${geohash} at ${point.lat}, ${point.lng}`)
+          return geohash
+        }
+      }
+    }
+  }
+}
+
+module.exports = new XyoWebsocketNotify()
--- src/repository/dynamodb/xyo-dynamo-migrator.ts
+++ src/repository/dynamodb/xyo-dynamo-migrator.ts
@@ -1,8 +1,10 @@
 import { XyoArchivistDynamoRepository } from './xyo-dynamo-archivist-repository'
-import { XyoBoundWitness, XyoSha256 } from '@xyo-network/sdk-core-nodejs'
+import { XyoBoundWitness, XyoSha256, XyoObjectSchema, gpsResolver } from '@xyo-network/sdk-core-nodejs'
 import { XyoBase } from '@xyo-network/sdk-base-nodejs'
 import bs58 from 'bs58'
+import ngeohash from 'ngeohash'
 import { ArchivistAbsorber } from '../../absorb/archivist-absorber'
+import { Client, BulkIndexDocumentsParams } from 'elasticsearch'
 
 const hasher = new XyoSha256()
 class Migrator extends XyoBase {
@@ -17,25 +19,83 @@ class Migrator extends XyoBase {
     const absorber = new ArchivistAbsorber(process.argv[2])
 
     while (true) {
-      // this.logInfo(`Migrating 50 blocks starting at offset hash: ${offset && bs58.encode(offset)}`)
-      const blocks = await absorber.readBlocks(50)
+      const blocks = await absorber.readBlocks(1000)
 
-      await Promise.all(blocks.map(async(block) => {
-        try {
-          const bw = new XyoBoundWitness(block)
-          const hash = bw.getHash(hasher).getAll().getContentsCopy()
-          await this.db.addOriginBlock(hash, block)
-        } catch (e) {
-          this.logError(`Error adding block ${e}`)
+      const client = new Client({
+        host: 'https://search-xyo-archivist-geohash-dswz22xbqpqxte3fls5fzpuf5u.us-east-1.es.amazonaws.com'
+      })
+
+      const bulks: any[] = []
+
+      blocks.forEach((block) => {
+        const bw = new XyoBoundWitness(block)
+        const geohash = getGeohash(bw)
+
+        if (geohash) {
+          const hash = bs58.encode(bw.getHash(hasher).getAll().getContentsCopy())
+          bulks.push({ index:  { _index: 'geohash', _type: 'bound_witness', _id: hash } })
+          bulks.push({
+            geohash,
+            g1: geohash[0],
+            g2: geohash[0] + geohash[1],
+            g3: geohash[0] + geohash[1] + geohash[2],
+            g4: geohash[0] + geohash[1] + geohash[2] + geohash[3],
+            g5: geohash[0] + geohash[1] + geohash[2] + geohash[3] + geohash[4],
+            g6: geohash[0] + geohash[1] + geohash[2] + geohash[3] + geohash[4]  + geohash[5],
+          })
         }
-      }))
+      })
 
-      if (blocks.length < 49) {
-        this.logInfo(`Finished migration ${blocks.length}`)
-        break
+      const insert: BulkIndexDocumentsParams = {
+        index: 'geohash',
+        body: bulks
       }
+
+      await new Promise((resolve, reject) => {
+        client.bulk(insert, (e, r) => {
+          if (e) {
+            reject(e)
+          }
+
+          resolve(r)
+        })
+      })
     }
 
+    // while (true) {
+    //   // this.logInfo(`Migrating 50 blocks starting at offset hash: ${offset && bs58.encode(offset)}`)
+    //   const blocks = await absorber.readBlocks(50)
+
+    //   await Promise.all(blocks.map(async(block) => {
+    //     try {
+    //       const bw = new XyoBoundWitness(block)
+    //       const hash = bw.getHash(hasher).getAll().getContentsCopy()
+    //       await this.db.addOriginBlock(hash, block)
+    //     } catch (e) {
+    //       this.logError(`Error adding block ${e}`)
+    //     }
+    //   }))
+
+    //   if (blocks.length < 49) {
+    //     this.logInfo(`Finished migration ${blocks.length}`)
+    //     break
+    //   }
+    // }
+
+  }
+}
+
+const getGeohash = (boundWitness: XyoBoundWitness): string | undefined => {
+  for (const party of boundWitness.getHeuristics()) {
+    for (const huerestic of party) {
+
+      if (huerestic.getSchema().id === XyoObjectSchema.GPS.id) {
+        const point = gpsResolver.resolve(huerestic.getAll().getContentsCopy()).value
+        const geohash = ngeohash.encode(point.lat, point.lng)
+        // this.logInfo(`Adding geohash: ${geohash} at ${point.lat}, ${point.lng}`)
+        return geohash
+      }
+    }
   }
 }
 
@@ -47,4 +107,4 @@ async function main() {
 
 }
 
-main()
+// main()
